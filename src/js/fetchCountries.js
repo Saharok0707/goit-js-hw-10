53397818d4122c079bf126a72297fc65575cd4b4
Saharok0707 <saharok0707@gmail.com>
2021-12-15 20:56:05 +0200

    ввввв

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -42,14 +42,3 @@ export default class RestCountriesAPI {
     }
 }
 
-// const BASE_URL = 'https://restcountries.com/v2/name';
-
-// export default function fetchCountries(searchQuery) {
-//     const url = `${BASE_URL}/${searchQuery}`;
-
-//     return fetch(url)
-//       .then(response => response.json())
-//       .then((countries ) => {
-//         return countries;
-//       });
-//   }
