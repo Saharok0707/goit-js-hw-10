7ba6443469759965f91956ab04720983ee496542
Saharok0707 <saharok0707@gmail.com>
2021-12-16 21:58:08 +0200

    hgsjfs

--- src/index.js
+++ src/index.js
@@ -1,108 +1,45 @@
 import './css/styles.css';
-import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import RestCountriesAPI from "./js/fetchCountries";
-import { debounce } from 'lodash';
-export { general };
+import { fetchCountries } from './js/fetchCountries';
+import countryCard from './templates/country.hbs';
+import countryList from './templates/countryList.hbs';
 
 const DEBOUNCE_DELAY = 300;
-const general = {
-  searchBox: document.querySelector("input#search-box"),
-  countryList: document.querySelector(".country-list"),
-  countryInfo: document.querySelector(".country-info"),
-};
-let name = '';
-const restCountriesAPI = new RestCountriesAPI();
+const debounce = require('lodash.debounce');
 
-general.searchBox.addEventListener("input",
-  debounce(onSearch, DEBOUNCE_DELAY, { trailing: true }));
 
-function onSearch(e) {
-  e.preventDefault();
-  name = e.path[0].value.trim();
-  general.countryInfo.innerHTML = '';
-  general.countryList.innerHTML = '';
+const inputEl = document.querySelector('#search-box');
+const countriesListEl = document.querySelector('.country-list');
+const cardContainerEl = document.querySelector('.country-info');
 
-  if (name !== "") {
-    restCountriesAPI.name = name;
-    restCountriesAPI.fetchCountries(name).then(countryGetInfo => {
-      renderCountryInfo(countryGetInfo);
-    });
-  }
-};
 
-function renderCountryInfo(countryGetInfo) {
-    for (const key in countryGetInfo) {
-        if (Object.hasOwnProperty.call(countryGetInfo, key)) {
-            const element = countryGetInfo[key];
-            console.log(element);
-            console.log(countryGetInfo);
 
-            const { name, capital, population, flags, languages }
-                = element;
-            console.log(countryGetInfo.length);
+const onCountriesFetch = (event) => {
+    const inputValue = event.target.value.trim();
+    if (inputValue.length === 0) {
+        countriesListEl.innerHTML = "";
+        return;
+    } else {
+        fetchCountries(inputValue)
+        .then(onMarkupCountryCard)
+            .catch( () => {
+                countriesListEl.innerHTML = "";
+                Notify.failure('Oops, there is no country with that name');
+            });
+    };
+} 
 
-            if (countryGetInfo.length > 1 && countryGetInfo.length < 10) {
-                general.countryInfo.innerHTML = '';
-                general.countryList.innerHTML = '';
-                return AddCountryList(countryGetInfo);
-            }
+inputEl.addEventListener('input', debounce(onCountriesFetch, DEBOUNCE_DELAY));
 
-            if (countryGetInfo.length === 1) {
-                general.countryInfo.innerHTML = '';
-                general.countryList.innerHTML = '';
-                return AddCountryFullInfo(countryGetInfo);
-            };
 
-            if (countryGetInfo.length >= 10) {
-                general.countryInfo.innerHTML = '';
-                general.countryList.innerHTML = '';
-            }
-
-        
-    function AddCountryFullInfo(countryGetInfo) {
-        return general.countryInfo.insertAdjacentHTML("beforeend", `<div class="flag-country-block">
-        <img
-          class="flag"
-          src="${flags.svg}"
-          alt="flag"
-        />
-        <h1>${name.official}</h1>
-      </div>
-      <ul class="country-info-details">
-        <li class="country-info-item">
-          <h2>Capital:</h2>
-          <span class="info-value">${capital}</span>
-        </li>
-        <li class="country-info-item">
-          <h2>Population:</h2>
-          <span class="info-value">${population}</span
-          >
-        </li>
-        <li class="country-info-item">
-          <h2>Languages:</h2>
-          <span class="info-value">${Object.values(languages)}</span
-          >
-        </li>
-      </ul>`);
+// Добавляем разметку
+const onMarkupCountryCard = (countries) => {
+    if (countries.length > 10) {
+        cardContainerEl.innerHTML = "";
+        return Notify.info('Too many matches found. Please enter a more specific name.');
+    } if (countries.length === 1) {
+        cardContainerEl.innerHTML = countryCard(countries[0]);
+    } else {
+        countriesListEl.innerHTML = countryList(countries);
     }
-    }
-  };
-};
-
-function AddCountryList(countryGetInfo) {
-  const markup = countryGetInfo.map((item) => {
-    console.log(item);
-
-    return `<li class="country-list-item">
-        <img
-          class="flag-list"
-          src="${item.flags.svg}"
-          alt="flag"
-          />
-        <h2 class="list-item-h2">${item.name.official}</h2>
-      </li>`
-  }).join("");
-
-  general.countryList.insertAdjacentHTML("beforeend", markup);
-};
+}
